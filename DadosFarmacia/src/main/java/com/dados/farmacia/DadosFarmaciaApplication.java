0efe0b22c080547c43c6207c3667fbfc6429b568
João Orlando <joaoorlandojr7@gmail.com>
2022-08-02 23:47:40 -0300

    CRUD Farmácia Completa

---
+++ DadosFarmacia/src/main/java/com/dados/farmacia/DadosFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.dados.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosFarmaciaApplication.class, args);
+	}
+
+}
